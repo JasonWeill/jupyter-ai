990550603e3e1cfb3be576a7f0df138da563b02c
Jason Weill <jweill@amazon.com>
2023-05-01 18:55:22 -0700

    Consistent styling for command descriptions

--- packages/jupyter-ai-magics/jupyter_ai_magics/magics.py
+++ packages/jupyter-ai-magics/jupyter_ai_magics/magics.py
@@ -94,11 +94,11 @@ AI_COMMANDS = {
     "help": ("Display a list of supported commands", "%ai help"),
     "list": ("Display a list of models that you can use (optionally, for a single provider)",
         "%ai list [PROVIDER_ID]"),
-    "register": ("Add a custom LangChain chain or alias to a model for use with %ai magic commands.",
+    "register": ("Add a custom LangChain chain or alias to a model for use with %ai magic commands",
         REGISTER_COMMAND_USAGE),
-    "delete": ("Delete a previously registered name that points to a chain or model.",
+    "delete": ("Delete a previously registered name that points to a chain or model",
         DELETE_COMMAND_USAGE),
-    "update": ("Update a previously registered name that points to a chain or model.",
+    "update": ("Update a previously registered name that points to a chain or model",
         UPDATE_COMMAND_USAGE)
 }
 
